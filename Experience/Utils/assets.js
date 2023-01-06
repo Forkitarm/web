d94226e8d9ec08fdb8ad58664735ea26ac2f075d
Dominik Gaglio <dominikgaglio@RFC.fritz.box>
2023-01-06 12:05:48 +0100

    added brand video example

--- Experience/Utils/assets.js
+++ Experience/Utils/assets.js
@@ -2,7 +2,7 @@ export default [
     {
         name: 'room',
         type: 'glbModel',
-        path: '/models/originchanged2.glb',
+        path: '/models/final.glb',
     },
     {
         name: 'screen',
